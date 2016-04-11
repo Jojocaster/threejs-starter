46599ac93d0aace3871820d7e5e57b7d0f42025d
JordanMachado <jordan@stinkdigital.com>
2016-04-11 13:18:14 +0200

    Added events / Wagner / Example / Editor config

--- app/Webgl.js
+++ app/Webgl.js
@@ -1,56 +1,105 @@
 import THREE from 'three';
 window.THREE = THREE;
+import WAGNER from '@superguigui/wagner';
+// const VignettePass = require('./postprocessing/vignette-white/VignettePass');
+const FXAAPass = require('@superguigui/wagner/src/passes/fxaa/FXAAPASS');
+const VignettePass = require('@superguigui/wagner/src/passes/vignette/VignettePass');
 import Cube from './objects/Cube';
 
 export default class Webgl {
-    constructor(width, height) {
-        this.params = {
-            usePostprocessing: false,
-        };
+  constructor(params) {
+    this.params = {
+      postProcessing: params.postProcessing || false,
+      keyboard: params.keyboard || false,
+      mouse: params.mouse || false,
+      touch: params.touch || false,
+    };
 
-        this.scene = new THREE.Scene();
+    this.scene = new THREE.Scene();
 
-        this.camera = new THREE.PerspectiveCamera(50, width / height, 1, 1000);
-        this.camera.position.z = 100;
+    this.camera = new THREE.PerspectiveCamera(50, params.size.width / params.size.height, 1, 1000);
+    this.camera.position.z = 100;
 
-        this.renderer = new THREE.WebGLRenderer();
-        this.renderer.setSize(width, height);
-        this.renderer.setClearColor(0x262626);
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize(params.size.width, params.size.height);
+    this.renderer.setClearColor(0x262626);
 
-        this.composer = null;
-        this.initPostprocessing();
+    this.composer = null;
+    this.initPostprocessing();
 
-        this.cube = new Cube();
-        this.cube.position.set(0, 0, 0);
-        this.scene.add(this.cube);
+    this.cube = new Cube();
+    this.cube.position.set(0, 0, 0);
+    this.scene.add(this.cube);
+  }
+
+  initPostprocessing() {
+    this.composer = new WAGNER.Composer(this.renderer);
+    this.composer.setSize(window.innerWidth, window.innerHeight);
+    window.composer = this.composer;
+
+    // Passes
+    this.fxaaPass = new FXAAPass();
+    this.vignettePass = new VignettePass();
+
+  }
+  render() {
+    if (this.params.postProcessing) {
+      this.composer.reset();
+      this.composer.render(this.scene, this.camera);
+
+      // Passes
+      this.composer.pass(this.fxaaPass);
+      this.composer.pass(this.vignettePass);
+      this.composer.toScreen();
+
+    } else {
+      this.renderer.render(this.scene, this.camera);
     }
 
-    initPostprocessing() {
-        if (!this.params.usePostprocessing) {
-            return;
-        }
-
-        /* Add the effect composer of your choice */
+    this.cube.update();
+  }
+  // Events
+  resize(width, height) {
+    if (this.composer) {
+      this.composer.setSize(width, height);
     }
 
-    resize(width, height) {
-        if (this.composer) {
-            this.composer.setSize(width, height);
-        }
+    this.camera.aspect = width / height;
+    this.camera.updateProjectionMatrix();
 
-        this.camera.aspect = width / height;
-        this.camera.updateProjectionMatrix();
+    this.renderer.setSize(width, height);
+  }
+  keyPress() {
+    if (!this.params.keyboard) return;
+    console.log('keyPress');
+  }
+  keyDown() {
+    if (!this.params.keyboard) return;
+    console.log('keyDown');
+  }
+  keyUp() {
+    if (!this.params.keyboard) return;
+    console.log('keyUp');
+  }
+  click() {
+    if (!this.params.mouse) return;
+    console.log('click');
+  }
+  mouseMove() {
+    if (!this.params.mouse) return;
+    console.log('mousemove');
+  }
+  touchStart() {
+    if (!this.params.touch) return;
+    console.log('touchstart');
+  }
+  touchEnd() {
+    if (!this.params.touch) return;
+    console.log('touchend');
+  }
+  touchMove() {
+    if (!this.params.touch) return;
+    console.log('touchmove');
+  }
 
-        this.renderer.setSize(width, height);
-    }
-
-    render() {
-        if (this.params.usePostprocessing) {
-            console.warn('WebGL - No effect composer set.');
-        } else {
-            this.renderer.render(this.scene, this.camera);
-        }
-
-        this.cube.update();
-    }
 }
--- app/index.js
+++ app/index.js
@@ -1,31 +1,92 @@
 import Webgl from './Webgl';
+import deviceType from 'ua-device-type';
+import domReady from 'domready';
 import raf from 'raf';
 import dat from 'dat-gui';
 import 'gsap';
 
+// Vars
+window.DEBUG = true;
+let device;
 let webgl;
 let gui;
 
-// webgl settings
-webgl = new Webgl(window.innerWidth, window.innerHeight);
-document.body.appendChild(webgl.renderer.domElement);
-
-// GUI settings
-gui = new dat.GUI();
-gui.add(webgl.params, 'usePostprocessing');
-
-// handle resize
-window.addEventListener('resize', resizeHandler);
-
-// let's play !
-animate();
-
-function resizeHandler() {
-  webgl.resize(window.innerWidth, window.innerHeight);
-}
 
 function animate() {
   raf(animate);
-
   webgl.render();
 }
+
+// Events
+function resize() {
+  webgl.resize(window.innerWidth, window.innerHeight);
+}
+// KeyBoard
+function keyPress(e) {
+  webgl.keyPress(e);
+}
+function keyDown(e) {
+  webgl.keyDown(e);
+}
+function keyUp(e) {
+  webgl.keyUp(e);
+}
+// Mouse
+function click(e) {
+  webgl.click(e.clientX, e.clientY, e.timeStamp);
+}
+function mouseMove(e) {
+  webgl.mouseMove(e.touches);
+}
+// Touch
+function touchStart(e) {
+  webgl.touchStart(e.touches);
+}
+function touchEnd(e) {
+  webgl.touchEnd(e.touches);
+}
+function touchMove(e) {
+  webgl.touchMove(e.touches);
+}
+
+domReady(() => {
+  device = deviceType(navigator.userAgent);
+  document.querySelector('html').classList.add(device);
+
+  webgl = new Webgl({
+    size: {
+      width: window.innerWidth,
+      height: window.innerHeight,
+    },
+    keyboard: true,
+    mouse: true,
+  });
+  document.body.appendChild(webgl.renderer.domElement);
+
+  if (window.DEBUG) {
+    gui = new dat.GUI();
+    const webGLFolder = gui.addFolder('WebGL');
+    webGLFolder.add(webgl.params, 'postProcessing');
+    webGLFolder.add(webgl.params, 'keyboard');
+    webGLFolder.add(webgl.params, 'mouse');
+    webGLFolder.add(webgl.params, 'touch');
+    webGLFolder.open();
+  }
+
+  // Events
+  window.addEventListener('resize', resize);
+  // KeyBoard
+  window.addEventListener('keypress', keyPress);
+  window.addEventListener('keydown', keyDown);
+  window.addEventListener('keyup', keyUp);
+  // Mouse
+  window.addEventListener('click', click);
+  window.addEventListener('mousemove', mouseMove);
+  // Touch
+  window.addEventListener('touchstart', touchStart);
+  window.addEventListener('touchend', touchEnd);
+  window.addEventListener('touchmove', touchMove);
+
+  // let's start
+  animate();
+});
